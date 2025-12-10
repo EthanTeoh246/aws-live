f9f094453a6a86300a4fe6bf4713174210cd97f5
EthanTeoh246 <ethantjy@gmail.com>
2025-12-10 17:11:33 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.cv2e08oomuyw.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "12345678"
+customdb = "employee"
+custombucket = "ethanteohjunyew-employee"
+customregion = "us-east-1"
+
